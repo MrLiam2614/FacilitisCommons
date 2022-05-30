f06f8bc9f37974ca366490c56fae2c2755459e96
BuildTools <unconfigured@null.spigotmc.org>
2022-05-30 20:26:45 +0200

    Added Config

---
+++ src/main/java/me/mrliam2614/facilitiscommons/configs/Config.java
@@ -0,0 +1,55 @@
+package me.mrliam2614.facilitiscommons.configs;
+
+import lombok.Getter;
+import me.mrliam2614.facilitiscommons.utils.Messages;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class Config {
+    private final String source;
+    private final String end;
+    private File file;
+    private final JavaPlugin plugin;
+    @Getter
+    private FileConfiguration fileConfig;
+
+    public Config(String fileSource, JavaPlugin plugin) {
+        this.source = fileSource;
+        this.end = fileSource;
+        this.plugin = plugin;
+    }
+
+    public Config(String fileSource, String endFile, JavaPlugin plugin) {
+        this.source = fileSource;
+        this.end = endFile;
+        this.plugin = plugin;
+    }
+
+    public void init() {
+        file = new File(plugin.getDataFolder(), end);
+        if (!file.exists()) {
+            File sourceF = new File(plugin.getDataFolder(), source);
+            file.getParentFile().mkdirs();
+            plugin.saveResource(source, false);
+            sourceF.renameTo(file);
+        }
+
+        fileConfig = new YamlConfiguration();
+        try {
+            fileConfig.load(file);
+        } catch (Exception e) {
+            Messages.sendError(plugin, e.toString());
+        }
+    }
+
+    public void save() {
+        try {
+            fileConfig.save(file);
+        } catch (Exception e) {
+            Messages.sendError(plugin, e.toString());
+        }
+    }
+}
